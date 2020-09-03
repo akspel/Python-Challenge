e86f844d4a3b92628bd483296997799b22be3a06
Alexis Speliotis <akspeliotis@gmail.com>
2020-09-03 16:53:15 -0700

    updated csv file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,8 @@
-file = 'Resources/budget_data.csv'
-with open(file, 'r') as text:
-    line = text.read()
-    print(line)
+import os
+csvpath = os.path.join('Resources', 'budget_data.csv')
+import csv
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    for row in csvreader:
+        print(row)
