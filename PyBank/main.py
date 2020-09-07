49612807683f7035abcb3687d7bf10271cf2b402
Alexis Speliotis <akspeliotis@gmail.com>
2020-09-07 12:07:15 -0700

    added greatest increase and decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,15 +13,23 @@ with open(csvpath, newline='') as csvfile:
     greatest_increase = 0
     greatest_decrease = 0
     previous_amount = int(first_month[1])
+# creating loop of csv    
     for row in csvreader:
         months = months + 1
-        amount = amount + int(row[1])   
+        amount = amount + int(row[1]) 
+        difference = previous_amount - int(row[1])
+# looping to get increases and decreases        
+        if difference > greatest_increase:
+            greatest_increase = difference
+        if difference < greatest_decrease:
+            greatest_decrease = difference
+        previous_amount = int(row[1])          
 # calculating total months
     print(months)  
 # calculating total amount of Profit/Losses
     print(amount)
 # calculating average of changes in Profit/Losses
 # Find the greatest increase in profits (date and amount)
-  
+    print(greatest_increase)
 # Find the greatest decrease in losses
-              
+    print(greatest_decrease)          
