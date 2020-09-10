c7720d281ef9d53f4b16bf0cfe8ebcacd55b11ba
Alexis Speliotis <akspeliotis@gmail.com>
2020-09-09 17:23:34 -0700

    candidate list

--- PyPoll/pypollmain.py
+++ PyPoll/pypollmain.py
@@ -16,9 +16,20 @@ with open(csvpath, newline='') as csvfile:
  # create for loop
     for row in csvreader:
         total_votes = total_votes + 1
+        candidate_in = (row[2])
 
+        # finding the different candidates
+        if candidate_in in candidates_unique:
+            candidate_index = candidates_unique.index(candidate_in)
+            candidates_vote_count[candidate_index] = candidates_vote_count[candidate_index] + 1
+        else:
+            candidates_unique.append(candidate_in)
+            candidates_vote_count.append(1)   
 
 
 
  # The total numbers of votes cast
-        print(total_votes)             
+        print(f'Total Votes: {total_votes}')
+ # A complete list of the candidates who recieved votes
+        print(f'Each Candidate: {candidate_in}')
+
